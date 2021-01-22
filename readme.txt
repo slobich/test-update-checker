38b4e411b82acca2ae8f13f8ecdc4176683c2554
slobich <slobich@gmail.com>
2021-01-22 10:17:40 +0100

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,10 @@
+=== Test Update Checker ===
+Test "Plugin Update Checker Library".
+
+== Installation ==
+Visit [the repo](https://github.com/slobich/test-update-checker).
+
+== Changelog ==
+
+= 1.0 =
+* First Release.
---
+++ test-update-checker.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Test Update Checker
+ * Description: Test "Plugin Update Checker Library".
+ * Version: 1.0
+ */
+
+defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
+
+require 'plugin-update-checker/plugin-update-checker.php';
+
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+  'https://github.com/slobich/test-update-checker',
+  __FILE__, //Full path to the main plugin file or functions.php.
+  'test-update-checker'
+);
