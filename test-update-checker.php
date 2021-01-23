41b95c5d0a984ae57568c8a4a6f531dffa378f35
slobich <slobich@gmail.com>
2021-01-23 12:52:33 +0100

    Remove Plugin Update Checker library

--- test-update-checker.php
+++ test-update-checker.php
@@ -8,10 +8,3 @@
 defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
 
 require_once 'action-scheduler/action-scheduler.php';
-require 'plugin-update-checker/plugin-update-checker.php';
-
-$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-  'https://github.com/slobich/test-update-checker',
-  __FILE__, //Full path to the main plugin file or functions.php.
-  'test-update-checker'
-);
