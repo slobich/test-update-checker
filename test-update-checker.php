2899afd9ce881892a47a0515aab5c4f261ac0fbe
slobich <slobich@gmail.com>
2021-01-23 11:38:18 +0100

    Remove everything from plugin root file

--- test-update-checker.php
+++ test-update-checker.php
@@ -4,13 +4,3 @@
  * Description: Test "Plugin Update Checker Library".
  * Version: 3.0
  */
-
-defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
-
-require 'plugin-update-checker/plugin-update-checker.php';
-
-$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-  'https://github.com/slobich/test-update-checker',
-  __FILE__, //Full path to the main plugin file or functions.php.
-  'test-update-checker'
-);
